c4eddd7ae3981b98aedd8b800dde9e8c1bde1d53
SSasha88 <d1rus1853@gmail.com>
2024-11-13 21:28:17 +0300

    Выполнено.

--- module_1_6.py
+++ module_1_6.py
@@ -1,6 +1,5 @@
  # Работа со словарями:
 my_dict = {'Sasha':1988,'Ivan':1985,'Olga':1991}
-print(type(my_dict))
 print('Dictionary:', my_dict)
 print('Existing value "Sasha":', my_dict['Sasha'])
 print('Not existing value "Marina":',my_dict.get('Marina'))
@@ -10,8 +9,8 @@ print('Deleted value "Sasha":', delete)
 print('Modified dictionary:',my_dict)
  # Работа с множествами:
 my_set = {1,1,3,3,4,4,'one','two','one'}
-print(type(my_set))
-print('Set',my_set)
+print('Set:',my_set)
 my_set.remove(4)
-my_set.add('zero')
+plus_my_set=['zero','study']
+my_set.update(plus_my_set)
 print('Modified set:',my_set)
