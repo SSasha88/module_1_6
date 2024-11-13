d0201c0852ac499a52abc5e286b9f00ae1110c00
SSasha88 <d1rus1853@gmail.com>
2024-11-13 18:26:25 +0300

    Не могу добавить сразу несколько значений в множество??

--- module_1_6.py
+++ module_1_6.py
@@ -1,4 +1,6 @@
+ # Работа со словарями:
 my_dict = {'Sasha':1988,'Ivan':1985,'Olga':1991}
+print(type(my_dict))
 print('Dictionary:', my_dict)
 print('Existing value "Sasha":', my_dict['Sasha'])
 print('Not existing value "Marina":',my_dict.get('Marina'))
@@ -6,4 +8,10 @@ my_dict.update({'Artem':1973,'Nikita':2001})
 delete = my_dict.pop('Sasha')
 print('Deleted value "Sasha":', delete)
 print('Modified dictionary:',my_dict)
-
+ # Работа с множествами:
+my_set = {1,1,3,3,4,4,'one','two','one'}
+print(type(my_set))
+print('Set',my_set)
+my_set.remove(4)
+my_set.add('zero')
+print('Modified set:',my_set)
