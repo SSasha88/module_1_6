c553faede0de309ccddc10a89df8984e8f16928b
SSasha88 <d1rus1853@gmail.com>
2024-11-13 17:45:16 +0300

    Готово

--- module_1_6.py
+++ module_1_6.py
@@ -1,1 +1,9 @@
-л
+my_dict = {'Sasha':1988,'Ivan':1985,'Olga':1991}
+print('Dictionary:', my_dict)
+print('Existing value "Sasha":', my_dict['Sasha'])
+print('Not existing value "Marina":',my_dict.get('Marina'))
+my_dict.update({'Artem':1973,'Nikita':2001})
+delete = my_dict.pop('Sasha')
+print('Deleted value "Sasha":', delete)
+print('Modified dictionary:',my_dict)
+
